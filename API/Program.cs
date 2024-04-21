3a93e9c8efcd641db7fc5c7a7b86194a35caaa86
Thomas Ngo <tngo0508@gmail.com>
2024-04-21 14:05:57 -0700

    first commit

---
+++ API/Program.cs
@@ -0,0 +1,23 @@
+using API;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opts => {
+    opts.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+app.Run();
